c38cab06b69705839c33f4ebb9276223560cdee7
bellinux <bellinuxcom@gmail.com>
2023-12-07 17:12:43 -0300

    Update scriptsm.js

--- scriptsm.js
+++ scriptsm.js
@@ -392,7 +392,7 @@ function upFunction(){
 let pointIndex=0;
 let coordinates="timestamp;ghostX;ghostY;targetX;targetY\n";
 let speedTargetMS=272;
-speedTargetMS=30;
+//speedTargetMS=30;
 function iterateLines(){
 	setTimeout(function(){ 
 	
